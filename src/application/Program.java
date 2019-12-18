e0b63115a8c169c111c0b7d3952cd6dad8324372
Lenine Pestana <leninepestana@gmail.com>
2019-12-18 19:26:02 +0000

    Project with comparable class

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,32 +8,36 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+import entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		List<String> list = new ArrayList<>();
+		//List<String> list = new ArrayList<>();
+		List<Employee> employee = new ArrayList<Employee>();
+		
 		String path = "c:\\temp\\comparable.txt";
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			String compCsv = br.readLine();
+			while (compCsv != null) {
+				String[] fields = compCsv.split(",");
+				employee.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				compCsv = br.readLine();
 			}
 			
-			Collections.sort(list);
-			for (String s : list) {
-				System.out.println(s);
+			Collections.sort(employee);
+			for (Employee emp : employee) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
 			}
 			
 		} catch (IOException e) {
-			e.getMessage();
+			System.out.println(e.getMessage());
 		}
 		sc.close();
 	}
-
 }
